c718cda1f6aa9edae7b3512e5ea86f7583422824
e.alcobaca <e.alcobaca@gmail.com>
2018-11-22 18:56:43 -0200

    install and example

--- pymfe/utils/install.py
+++ pymfe/utils/install.py
@@ -1,19 +1,17 @@
 from rpy2.robjects.vectors import StrVector
 from rpy2.robjects import r
-from rpy2.robjects.packages import importr
+from rpy2.robjects.packages import isinstalled, importr
 from pymfe.exceptions.exceptions import MfeOption
+from pymfe.exceptions.exceptions import RLibNotFound, MfeOption
 
 
 class Install(object):
     """ """
     __devtools_cran = "devtools"
     __mfe_cran = "mfe"
-    __ecol_cran = "EcoL"
+    __ecol_cran = "ECoL"
     __mfe_devtools = "rivolli/mfe"
     __ecol_devtools = "SmartDataAnalytics/ECoL"
-    __devtools = """ if (!require("devtools")) install.packages("devtools");
-                 devtools::install_github("{0}");
-                 """
     __source_opt = {"cran", "devtools"}
     __mirror_opt = "`mirror` should be > 0, see more information here --> https://cran.r-project.org/mirrors.html"
 
@@ -28,47 +26,61 @@ class Install(object):
 
 
     @classmethod
-    def devtoolsR(cls, mirror=1)
+    def devtoolsR(cls, mirror=1):
         package = cls.__devtools_cran
-        cls.installr(package, cls.__source_opt[0], mirror)
+        cls.installR(package, "cran", mirror)
 
 
     @classmethod
     def mfeR(cls, source="cran", mirror=1):
         package = None
-        if source == cls.__source_opt[0]:
+        if source == "cran":
             package = cls.__mfe_cran
-        elif source == cls.__source_opt[1]:
+        elif source == "devtools":
             package = cls.__mfe_devtools
 
-        cls.installr(package, source, mirror)
+        cls.installR(package, source, mirror)
 
 
     @classmethod
     def ecolR(cls, source="cran", mirror=1):
         package = None
-        if source == cls.__source_opt[0]:
+        if source == "cran":
             package = cls.__ecol_cran
-        elif source == cls.__source_opt[1]:
+        elif source == "devtools":
             package = cls.__ecol_devtools
 
-        cls.installr(package, source, mirror)
+        cls.installR(package, source, mirror)
 
 
     @classmethod
-    def installr(cls, package, source="cran", mirror=1):
-        if source == cls.__source_opt[0]:
+    def installedR(cls, package):
+        return isinstalled(package)
+
+
+    @classmethod
+    def installR(cls, package, source="cran", mirror=1):
+        if source == "cran":
             utils = importr('utils')
             utils.chooseCRANmirror(ind=mirror)
-            utils.install_packages(StrVector(package))
-        elif source == cls.__source_opt[1]:
+
+            if isinstance(package, list):
+                utils.install_packages(StrVector(package))
+            else:
+                utils.install_packages(package)
+
+        elif source == "devtools":
             # verify if devtools is instelled
             exist = isinstalled(cls.__devtools_cran)
             if exist == True:
                 devtools = importr(cls.__devtools_cran)
             else:
-                # if devtools not found
                 raise RLibNotFound(cls.__devtools_cran)
-            devtools(StrVector(package))
+
+            if isinstance(package, list):
+                r("devtools::install_github({0})".format(StrVector(package).r_repr()))
+            else:
+                r("devtools::install_github(\'{0}\')".format(package))
+
         else:
             raise MfeOption("source", source, cls.get_all_source())
