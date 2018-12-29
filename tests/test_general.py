62b0a2c69126a970a1bc732fbb7066d9abf485b0
FelSiq <felipe.siqueira@usp.br>
2018-12-29 16:37:29 -0200

    Small changes ate tests due to pylint

--- tests/test_general.py
+++ tests/test_general.py
@@ -1,10 +1,12 @@
 """Test module for General class metafeatures."""
 import context
-from mfe.general import MFEGeneral
+from pymfe.general import MFEGeneral
 
 
-class TestGeneral(object):
+class TestGeneral:
+    """TestClass dedicated to test General metafeatures."""
 
     def test_dataset_0_inst_num(self):
+        """Test method for dataset #0 and metafeature "inst_num"."""
         dataset = context.DATASET_LIST[0]
         assert MFEGeneral.inst_num(dataset) == 1000
