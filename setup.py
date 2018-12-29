232d11c3cc0f7f639411a9344ee0b2b4b549d806
FelSiq <felipe.siqueira@usp.br>
2018-12-29 16:36:31 -0200

    Small fix due to pylint at setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 # -*- conding: utf-8 -*-
+"""Setup for pymfe package."""
 import setuptools
 
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setuptools.setup(
-    name="pyMFE",
+    name="pymfe",
     version="0.0.1",
     author="Edesio Alcobaça, Felipe Alves Siqueira",
     author_email="[email?], felipe.siqueira@usp.br",
     description="Metafeature extractor",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/.../pyMFE",
     packages=setuptools.find_packages(),
