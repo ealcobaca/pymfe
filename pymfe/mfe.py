d5308f3154777766c5c1dbc51fc740e6292281a3
FelSiq <felipe.siqueira@usp.br>
2018-12-29 14:19:45 -0200

    Changed path

---
+++ pymfe/mfe.py
@@ -0,0 +1,13 @@
+"""Main module for extracting metafeatures from datasets.
+
+Todo:
+    - Improve documentation.
+    - Implement MFE class.
+"""
+
+
+def MFE(object):
+
+    def __init__(self) -> None:
+        """To do this documentation."""
+        pass
