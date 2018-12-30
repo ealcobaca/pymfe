0dfac305d118a60f5cff05a103e8efc69c2d8c8c
FelSiq <felipe.siqueira@usp.br>
2018-12-30 17:29:35 -0200

    Small improvement on general.py module

--- python/pymfe/general.py
+++ python/pymfe/general.py
@@ -1,18 +1,38 @@
 """Module dedicated to extraction of General Metafeatures.
 
-Todo:
-    - Implement all metafeatures.
-    - Improve documentation.
+Notes:
+    More description about the metafeatures implemented here,
+    check out [1].
 
 References:
-    1. "Towards Reproducible Empirical Research in Meta-Learning",
+    [1] "Towards Reproducible Empirical Research in Meta-Learning",
         Rivolli et al. URL: https://arxiv.org/abs/1808.10406
+
+Todo:
+    * Implement all metafeatures.
+    * Improve documentation.
+
 """
 from typing import Union, Iterable
+import numpy as np
 
 
 class MFEGeneral:
     """General-type Metafeature extractor."""
+
+    FEATURES = (
+        "attr_to_inst",
+        "cat_to_num",
+        "freq_class",
+        "inst_to_attr",
+        "nr_attr",
+        "nr_bin",
+        "nr_cat",
+        "nr_class",
+        "nr_inst",
+        "nr_num",
+        "num_to_cat",
+    )
 
     def __init__(self, features: Union[str, Iterable[str]] = "all") -> None:
         """Extracts general metafeatures from datasets.
@@ -24,6 +44,7 @@ class MFEGeneral:
         """
         self.features = features
 
-    def inst_num(self, dataframe):
-        """Returns number of instances (rows) in DataFrame."""
-        return dataframe.shape[0]
+    @staticmethod
+    def ft_nr_inst(X: np.array) -> int:
+        """Returns number of instances (rows) in dataset."""
+        return X.shape[0]
