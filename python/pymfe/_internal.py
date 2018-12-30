09b25cf9295248912584473a80b908db590245b6
FelSiq <felipe.siqueira@usp.br>
2018-12-29 22:14:12 -0200

    Still working on _internal module

--- python/pymfe/_internal.py
+++ python/pymfe/_internal.py
@@ -3,6 +3,8 @@
 Attributes:
     VALID_GROUPS (:obj:`tuple` of :obj:`str`): Supported type of
         metafeatures of pymfe.
+    VALID_SUMMARY (:obj:`tuple` of :obj:`str`): Supported summary
+        functions to combine metafeature values.
 """
 import collections
 
@@ -23,6 +25,53 @@ VALID_SUMMARY = (
 )
 
 
+def _check_value_in_group(value, group, wildcard="all"):
+    """Checks if a given value (or a collection) are in a group.
+
+    Args:
+        value (:obj:`Iterable` of :obj:`str` or :obj:`str`): value(s)
+            to be checked if are in the given group of strings.
+        group (:obj:`Iterable` of :obj:`str`): a group of strings.
+
+    Returns:
+        Tuples: in_group and not_in_group containing, respectivelly,
+        values that are in the given group and those that are not.
+        If no value is in either group, then this group will be None.
+
+    Raises:
+        TypeError: if 'value' is not a Iterable type or some of its
+            elements are not a 'str' type.
+    """
+
+    if not isinstance(value, collections.Iterable):
+        raise TypeError("Parameter type is not consistent.")
+
+    in_group, not_in_group = None, None
+
+    if isinstance(value, str):
+        value = value.lower()
+        if value == wildcard:
+            in_group = group
+
+        elif value in group:
+            in_group = (value,)
+
+        else:
+            not_in_group = (value,)
+
+    elif isinstance(value, collections.Iterable):
+        value = set(map(str.lower, value))
+
+        if wildcard in value:
+            in_group = group
+
+        else:
+            in_group = value.intersection(group)
+            not_in_group = value.difference(group)
+
+    return in_group, not_in_group
+
+
 def process_groups(groups):
     """Check if 'groups' argument from MFE.__init__ is correct.
 
@@ -40,50 +89,58 @@ def process_groups(groups):
         A tuple containing all valid group lower-cased identifiers.
 
     Raises:
-        TypeError: if `groups` is not a string "all", a Iterable
-            containing valid group identifiers as strings, is None or
-            is a empty Iterable.
-        ValueError: if a unknown group identifier is given.
+        TypeError: if 'groups' is neither a string 'all' nor a Iterable
+            containing valid group identifiers as strings.
+        ValueError: if 'groups' is None or is a empty Iterable or
+            if a unknown group identifier is given.
     """
-    unknown_groups = None
+    if not groups:
+        raise ValueError('"Groups" can not be None nor empty.')
 
-    if groups is None or not groups:
-        raise AttributeError('"Groups" can not be None nor empty.')
+    in_group, not_in_group = _check_value_in_group(groups, VALID_GROUPS)
 
-    if isinstance(groups, str):
-        groups = groups.lower()
-        if groups == "all":
-            return VALID_GROUPS
+    if not_in_group:
+        raise ValueError("Unknown groups: {0}".format(not_in_group))
 
-        if groups in VALID_GROUPS:
-            return (groups,)
-
-        unknown_groups = {groups}
-
-    elif isinstance(groups, collections.Iterable):
-        groups = set(map(str.lower, groups))
-
-        if "all" in groups:
-            return VALID_GROUPS
-
-        if groups.issubset(VALID_GROUPS):
-            return tuple(groups)
-
-        unknown_groups = groups.difference(VALID_GROUPS)
-
-    if unknown_groups is not None:
-        raise ValueError("Unknown groups: {0}".format(unknown_groups))
-
-    raise TypeError('"Groups" parameter type is not consistent.')
+    return in_group
 
 
 def process_summary(summary):
-    """Check if 'summary' argument from MFE.__init__ is correct."""
-    return summary
+    """Check if 'summary' argument from MFE.__init__ is correct.
+    Args:
+        summary (:obj:`Iterable` of :obj:`str` or a :obj:`str`): a
+            summary function or a list of these, which are used to
+            combine different calculations of the same metafeature.
+            The values must be one of the following:
+                - "mean": average of the values.
+                - "sd": standard deviation of the values.
+                - more to be implemented (...)
+
+    Raises:
+        TypeError: if 'summary' is neither a string 'all' nor a Iterable
+            containing valid group identifiers as strings.
+        ValueError: if 'summary' is None or is a empty Iterable or
+            if a unknown group identifier is given.
+
+    Returns:
+        A tuple containing all valid lower-cased summary functions.
+    """
+    if not summary:
+        raise ValueError('"Summary" can not be None nor empty.')
+
+    in_group, not_in_group = _check_value_in_group(summary, VALID_SUMMARY)
+
+    if not_in_group:
+        raise ValueError("Unknown groups: {0}".format(not_in_group))
+
+    return in_group
 
 
 def process_features(features):
-    """Check if 'features' argument from MFE.__init__ is correct."""
+    """Check if 'features' argument from MFE.__init__ is correct.
+
+    Needs to be implemented.
+    """
     return features
 
 
@@ -97,7 +154,7 @@ def check_data(X, y):
         Check "mfe.fit" method for more information.
 
     Returns:
-        X and y both casted to a np.array.
+        X and y both casted to a numpy.array.
     """
     if not isinstance(X, (np.array, list)):
         raise TypeError('"X" is neither "list" nor "np.array".')
