a1778b0de2665b0659aac0b14626dd3b02132603
FelSiq <felipe.siqueira@usp.br>
2018-12-30 22:44:45 -0200

    Sketch for MFE module classes

---
+++ python/pymfe/info_theory.py
@@ -0,0 +1,12 @@
+"""Module dedicated to extraction of Information Theory Metafeatures.
+
+Todo:
+    * Implement metafeatures.
+    * Improve documentation.
+"""
+
+
+class MFEInfoTheory:
+    """To do this documentation."""
+
+    # To do.
---
+++ python/pymfe/model_based.py
@@ -0,0 +1,12 @@
+"""Module dedicated to extraction of Model Based Metafeatures.
+
+Todo:
+    * Implement metafeatures.
+    * Improve documentation.
+"""
+
+
+class MFEModelBased:
+    """To do this documentation."""
+
+    # To do.
