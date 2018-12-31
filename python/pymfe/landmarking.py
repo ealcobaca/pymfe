ca25f6c479545e5354128152f8ef3336596be08e
FelSiq <felipe.siqueira@usp.br>
2018-12-30 22:44:45 -0200

    Sketch for MFE module classes

---
+++ python/pymfe/landmarking.py
@@ -0,0 +1,12 @@
+"""Module dedicated to extraction of Landmarking Metafeatures.
+
+Todo:
+    * Implement metafeatures.
+    * Improve documentation.
+"""
+
+
+class MFELandmarking:
+    """To do this documentation."""
+
+    # To do.
---
+++ python/pymfe/statistical.py
@@ -0,0 +1,12 @@
+"""Module dedicated to extraction of Statistical Metafeatures.
+
+Todo:
+    * Implement metafeatures.
+    * Improve documentation.
+"""
+
+
+class MFEStatistical:
+    """To do this documentation."""
+
+    # To do.
