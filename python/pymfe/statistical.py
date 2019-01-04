a76d7c9bb5fd04cbed5c7c8cb2fa4edc023219dc
FelSiq <felipe.siqueira@usp.br>
2019-01-04 00:21:26 -0200

    feature samples for summary testing.

--- python/pymfe/statistical.py
+++ python/pymfe/statistical.py
@@ -4,11 +4,15 @@ Todo:
     * Implement metafeatures.
     * Improve documentation.
 """
+from typing import Union
+
+import numpy as np
 
 
 class MFEStatistical:
     """To do this documentation."""
 
     @classmethod
-    def ft_test_statistical(cls):
-        return 0.0
+    def ft_mean(cls, X: np.ndarray) -> Union[np.ndarray, float]:
+        """Returns the mean value of each data column."""
+        return X.mean(axis=0)
