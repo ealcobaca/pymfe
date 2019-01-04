ffc929e101d4ace438ecff3ad4268664d122024c
FelSiq <felipe.siqueira@usp.br>
2019-01-04 00:21:26 -0200

    feature samples for summary testing.

--- python/pymfe/info_theory.py
+++ python/pymfe/info_theory.py
@@ -4,11 +4,41 @@ Todo:
     * Implement metafeatures.
     * Improve documentation.
 """
+from typing import Union, List
+
+import numpy as np
+import scipy
 
 
 class MFEInfoTheory:
     """To do this documentation."""
 
     @classmethod
-    def ft_test_infotheory(cls):
-        return 0.0
+    def _entropy(cls, values: Union[np.ndarray, List]) -> float:
+        """Calculate entropy within array 'values'."""
+        _, counts = np.unique(values, return_counts=True)
+        return scipy.stats.entropy(counts, base=2)
+
+    @classmethod
+    def ft_attr_ent(cls, X: Union[np.ndarray, List]) -> np.ndarray:
+        """Calculates entropy for each attribute."""
+        return np.apply_along_axis(
+            func1d=MFEInfoTheory._entropy,
+            axis=0,
+            arr=X)
+
+    @classmethod
+    def ft_class_ent(cls, y: Union[np.ndarray, List]) -> np.ndarray:
+        """Calculates class entropy.
+
+        If data has multiclassed instances, each column is interpreted
+        as a class.
+        """
+
+        if isinstance(y, np.ndarray):
+            y = np.array(y)
+
+        if len(y.shape) > 1:
+            return MFEInfoTheory.ft_attr_ent(y)
+
+        return MFEInfoTheory._entropy(y)
