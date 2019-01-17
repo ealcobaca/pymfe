891c71dcfe6585777d21dac5c592af36c7158f4e
FelSiq <felipe.siqueira@usp.br>
2019-01-17 19:55:31 -0200

    Updating test script for General metafeats.

--- python/tests/context.py
+++ python/tests/context.py
@@ -5,8 +5,12 @@ Font:
     link: https://docs.python-guide.org/writing/structure/
 
 Attributes:
-    TEST_PATH (str): Path for test datasets used in test procedures.
-    DATASET_LIST (list): Contains all test data used in test procedures.
+    TEST_PATH (:obj:`str`): Path for test datasets used in test procedures.
+
+    DATASET_LIST (:obj:`list`): Contains all test data used in test procedures.
+
+    TARGET_COL_NAMES (:obj:`tuple` of :obj:`str`): tuple of column names of
+        test datasets to be assumed as target columns.
 """
 import os
 import sys
@@ -20,6 +24,8 @@ for new_path in IMPORT_PATH_TUPLE:
         0, os.path.abspath(os.path.join(os.path.dirname(__file__), new_path)))
 
 import pymfe  # noqa: E402, F401
+
+TARGET_COL_NAMES = ("target", )
 
 
 def _get_test_data(path):
--- python/tests/test_general.py
+++ python/tests/test_general.py
@@ -15,87 +15,156 @@ from pymfe.mfe import MFE
 class TestGeneral:
     """TestClass dedicated to test General metafeatures."""
 
-    SUMMARY_MFE_R = (
-        "histogram",
-        "kurtosis",
-        "max",
-        "mean",
-        "median",
-        "min",
-        "quantiles",
-        "sd",
-        "skewness",
-        "var",
-    )
-    """Summary functions supported by the R version of MFE."""
+    EPSILON = 1.0e-6
+    """Small value to assert floating point correctness w/o numeric errors."""
 
-    @pytest.mark.parametrize(
-        "dt_id, fit_args, feat_name_py, method_args, feat_name_r",
-        (
-            (0, {}, "attr_to_inst", {}, "attrToInst"),
-            (1, {}, "attr_to_inst", {}, "attrToInst"),
-            (0, {}, "nr_inst", {}, "nrInst"),
-            (1, {}, "nr_inst", {}, "nrInst"),
-            (0, {"check_bool": False}, "cat_to_num", {}, "catToNum"),
-            (1, {}, "cat_to_num", {}, "catToNum"),
-            (0, {}, "nr_attr", {}, "nrAttr"),
-            (1, {}, "nr_attr", {}, "nrAttr"),
-            (0, {}, "nr_bin", {}, "nrBin"),
-            (1, {}, "nr_bin", {}, "nrBin"),
-            (0, {"check_bool": False}, "cat_to_num", {}, "catToNum"),
-            (1, {}, "cat_to_num", {}, "catToNum"),
-            (0, {}, "freq_class", {}, "freqClass"),
-            (1, {}, "freq_class", {}, "freqClass"),
-            (0, {}, "inst_to_attr", {}, "instToAttr"),
-            (1, {}, "inst_to_attr", {}, "instToAttr"),
-            (0, {"check_bool": False}, "nr_cat", {}, "nrCat"),
-            (1, {}, "nr_cat", {}, "nrCat"),
-            (0, {}, "nr_class", {}, "nrClass"),
-            (1, {}, "nr_class", {}, "nrClass"),
-            (0, {"check_bool": False}, "nr_num", {}, "nrNum"),
-            (1, {}, "nr_num", {}, "nrNum"),
-            (0, {"check_bool": False}, "num_to_cat", {}, "numToCat"),
-            (1, {}, "num_to_cat", {}, "numToCat"),
-        ),
-    )
-    def test_ft_attr_to_inst(
-            self,
-            dt_id,
-            fit_args,
-            feat_name_py,
-            method_args,
-            feat_name_r):
-        """Test method ``."""
-        dataset = context.DATASET_LIST[dt_id]
+    @classmethod
+    def _get_col_indexes(cls, dataset):
+        """Split columns of independent and target attributes.
 
-        ind_targ = dataset.columns == "target"
+        It is assumed that the target attribute of test datasets
+        is in ``context.TARGET_COL_NAME``.
+        """
+        ind_targ = np.isin(dataset.columns, context.TARGET_COL_NAMES)
         ind_attr = ~ind_targ
 
+        return ind_targ, ind_attr
+
+    @classmethod
+    def _get_val_py(cls, dataset, feat_name_py, ind_attr, ind_targ,
+                    method_args, fit_args, summary_name):
+        """Instantiate, Fit and Extract summarized metafeature from MFE."""
         X = dataset.iloc[:, ind_attr].values
         y = dataset.iloc[:, ind_targ].values
 
-        ext_names, ext_values = MFE(
+        _, res_mfe_py_vals = MFE(
             features=feat_name_py,
-            summary=TestGeneral.SUMMARY_MFE_R,
-            ).fit(X=X, y=y, **fit_args).extract(**method_args)
+            summary=summary_name,
+        ).fit(
+            X=X, y=y, **fit_args).extract(**method_args)
 
-        mfe = rpy2.robjects.packages.importr("mfe")
+        return res_mfe_py_vals
 
+    @classmethod
+    def _na_to_nan(cls, values):
+        """Convert any R ``NA`` object to :obj:`np.nan` within a list."""
+        return np.array([
+            np.nan if isinstance(x, rpy2.rinterface.NALogicalType) else x
+            for x in list(values)
+        ])
+
+    @classmethod
+    def _get_val_r(cls, dataset, feat_name_r, ind_attr, ind_targ,
+                   summary_name):
+        """Get summarized metafeature value from R mfe implementation."""
         rpy2.robjects.pandas2ri.activate()
+        rpy2.robjects.numpy2ri.activate()
 
         dataset_r = rpy2.robjects.pandas2ri.py2ri(dataset)
 
-        expected_val = mfe.general(
+        mfe = rpy2.robjects.packages.importr("mfe")
+
+        res_mfe_r = mfe.general(
             x=dataset_r.rx(ind_attr),
             y=dataset_r.rx(ind_targ),
-            features=feat_name_r)
+            features=feat_name_r,
+            summary=summary_name)
 
         rpy2.robjects.pandas2ri.deactivate()
 
-        expected_val = expected_val.rx2(feat_name_r)[0]
+        res_mfe_r_vals = TestGeneral._na_to_nan(res_mfe_r.rx2(feat_name_r))
 
-        if isinstance(expected_val, rpy2.rinterface.NALogicalType):
-            expected_val = np.nan
+        return res_mfe_r_vals
 
-        assert (ext_values[0] == expected_val
-                or all(np.isnan((ext_values[0], expected_val))))
+    @pytest.mark.parametrize(
+        ("dt_id, fit_args, feat_name_py,"
+         "method_args, feat_name_r, summary_name"),
+        (
+            (0, {}, "attr_to_inst", {}, "attrToInst", "all"),
+            (1, {}, "attr_to_inst", {}, "attrToInst", "all"),
+            (0, {}, "nr_inst", {}, "nrInst", "all"),
+            (1, {}, "nr_inst", {}, "nrInst", "all"),
+            (0, {
+                "check_bool": False
+            }, "cat_to_num", {}, "catToNum", "all"),
+            (1, {}, "cat_to_num", {}, "catToNum", "all"),
+            (0, {}, "nr_attr", {}, "nrAttr", "all"),
+            (1, {}, "nr_attr", {}, "nrAttr", "all"),
+            (0, {}, "nr_bin", {}, "nrBin", "all"),
+            (1, {}, "nr_bin", {}, "nrBin", "all"),
+            (0, {
+                "check_bool": False
+            }, "cat_to_num", {}, "catToNum", "all"),
+            (1, {}, "cat_to_num", {}, "catToNum", "all"),
+            (0, {}, "freq_class", {}, "freqClass", "mean"),
+            (1, {}, "freq_class", {}, "freqClass", "mean"),
+            (0, {}, "freq_class", {}, "freqClass", "max"),
+            (1, {}, "freq_class", {}, "freqClass", "max"),
+            (0, {}, "freq_class", {}, "freqClass", "min"),
+            (1, {}, "freq_class", {}, "freqClass", "min"),
+            (0, {}, "freq_class", {}, "freqClass", "skewness"),
+            (1, {}, "freq_class", {}, "freqClass", "skewness"),
+            (0, {}, "freq_class", {}, "freqClass", "kurtosis"),
+            (1, {}, "freq_class", {}, "freqClass", "kurtosis"),
+            (0, {}, "freq_class", {}, "freqClass", "quantiles"),
+            (1, {}, "freq_class", {}, "freqClass", "quantiles"),
+            (0, {}, "freq_class", {}, "freqClass", "var"),
+            (1, {}, "freq_class", {}, "freqClass", "var"),
+            (0, {}, "freq_class", {}, "freqClass", "median"),
+            (1, {}, "freq_class", {}, "freqClass", "median"),
+            (0, {}, "freq_class", {
+                "sd": {
+                    "ddof": 1
+                }
+            }, "freqClass", "sd"),
+            (1, {}, "freq_class", {
+                "sd": {
+                    "ddof": 1
+                }
+            }, "freqClass", "sd"),
+            (0, {}, "freq_class", {
+                "histogram": {
+                    "bins": 10,
+                    "normalize": True
+                }
+            }, "freqClass", "histogram"),
+            (1, {}, "freq_class", {
+                "histogram": {
+                    "bins": 10,
+                    "normalize": True
+                }
+            }, "freqClass", "histogram"),
+            (0, {}, "inst_to_attr", {}, "instToAttr", "all"),
+            (1, {}, "inst_to_attr", {}, "instToAttr", "all"),
+            (0, {
+                "check_bool": False
+            }, "nr_cat", {}, "nrCat", "all"),
+            (1, {}, "nr_cat", {}, "nrCat", "all"),
+            (0, {}, "nr_class", {}, "nrClass", "all"),
+            (1, {}, "nr_class", {}, "nrClass", "all"),
+            (0, {
+                "check_bool": False
+            }, "nr_num", {}, "nrNum", "all"),
+            (1, {}, "nr_num", {}, "nrNum", "all"),
+            (0, {
+                "check_bool": False
+            }, "num_to_cat", {}, "numToCat", "all"),
+            (1, {}, "num_to_cat", {}, "numToCat", "all"),
+        ),
+    )
+    def test_ft_attr_to_inst(self, dt_id, fit_args, feat_name_py, method_args,
+                             feat_name_r, summary_name):
+        """Compare metafeat. values against correspondent from R mfe."""
+        dataset = context.DATASET_LIST[dt_id]
+
+        ind_targ, ind_attr = TestGeneral._get_col_indexes(dataset)
+
+        res_mfe_py = TestGeneral._get_val_py(dataset, feat_name_py, ind_attr,
+                                             ind_targ, method_args, fit_args,
+                                             summary_name)
+
+        res_mfe_r = TestGeneral._get_val_r(dataset, feat_name_r, ind_attr,
+                                           ind_targ, summary_name)
+
+        assert (all(np.isnan(res_mfe_py) == np.isnan(res_mfe_r))
+                or all(res_mfe_py - res_mfe_r < TestGeneral.EPSILON))
