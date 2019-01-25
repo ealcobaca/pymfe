0aecc0602338c7fb5cae6cbbb22d6fe27ac47dc6
FelSiq <felipe.siqueira@usp.br>
2019-01-24 22:10:12 -0200

    Beta version of all info theoretic methods

--- python/pymfe/info_theory.py
+++ python/pymfe/info_theory.py
@@ -1,9 +1,4 @@
-"""Module dedicated to extraction of Information Theory Metafeatures.
-
-Todo:
-    * Implement metafeatures.
-    * Improve documentation.
-"""
+"""Module dedicated to extraction of Information Theory Metafeatures."""
 import typing as t
 import itertools
 
@@ -22,6 +17,19 @@ class MFEInfoTheory:
         return scipy.stats.entropy(counts, base=2)
 
     @classmethod
+    def _joint_prob_mat(cls, x: np.ndarray, y: np.ndarray) -> np.ndarray:
+        """Compute joint probability matrix P(a, b), a in x and b in y."""
+        x_vals = np.unique(x)
+        y_vals = np.unique(y)
+
+        joint_prob_mat = np.array([
+            sum((x == x_val) & (y == y_val))
+            for y_val, x_val in itertools.product(y_vals, x_vals)
+        ]).reshape((y_vals.size, x_vals.size)) / x.size
+
+        return joint_prob_mat
+
+    @classmethod
     def _conc(cls, x: np.ndarray, y: np.ndarray,
               epsilon: float = 1.0e-8) -> float:
         """Compute concentration coefficient between two arrays ``x`` and ``y``.
@@ -32,13 +40,7 @@ class MFEInfoTheory:
             epsilon (:obj:`float`, optional): small numeric value to
                 avoid division by zero.
         """
-        x_vals = np.unique(x)
-        y_vals = np.unique(y)
-
-        pij = np.array([
-            sum((x == x_val) & (y == y_val))
-            for y_val, x_val in itertools.product(y_vals, x_vals)
-        ]).reshape((y_vals.size, x_vals.size)) / x.size
+        pij = MFEInfoTheory._joint_prob_mat(x, y)
 
         isum = pij.sum(axis=1)
         jsum2 = sum(pij.sum(axis=0)**2.0)
@@ -50,7 +52,7 @@ class MFEInfoTheory:
 
     @classmethod
     def ft_attr_conc(cls, C: np.ndarray) -> np.ndarray:
-        """To do this doc."""
+        """Compute concentration coef. of each pair of distinct attributes."""
 
         _, num_col = C.shape
 
@@ -65,7 +67,7 @@ class MFEInfoTheory:
 
     @classmethod
     def ft_attr_ent(cls, C: np.ndarray) -> t.Union[np.ndarray, np.float]:
-        """Calculates entropy for each attribute."""
+        """Calculates Shannon entropy for each attribute."""
         try:
             return np.apply_along_axis(
                 func1d=MFEInfoTheory._entropy, axis=0, arr=C)
@@ -81,12 +83,12 @@ class MFEInfoTheory:
 
     @classmethod
     def ft_class_ent(cls, y: np.ndarray) -> t.Union[np.ndarray, np.float]:
-        """Calculates class entropy."""
+        """Calculates class Shannon entropy."""
         return MFEInfoTheory._entropy(y)
 
     @classmethod
     def ft_eq_num_attr(cls, C: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """To do this doc."""
+        """Number of attributes equivalent for a predictive task."""
         ent_class = MFEInfoTheory._entropy(y)
         mutual_info = MFEInfoTheory.ft_mut_inf(C, y)
 
@@ -96,7 +98,22 @@ class MFEInfoTheory:
 
     @classmethod
     def ft_joint_entropy(cls, C: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """To do this doc."""
+        """Calculate Joint entropy between each attribute and class."""
+
+        def joint_entropy(x: np.ndarray,
+                          y: np.ndarray,
+                          epsilon: float = 1.0e-8) -> float:
+            joint_prob_mat = MFEInfoTheory._joint_prob_mat(x, y) + epsilon
+
+            joint_entropy = np.multiply(joint_prob_mat,
+                                        np.log2(joint_prob_mat)).sum().sum()
+
+            return -1.0 * joint_entropy
+
+        joint_entropy = np.apply_along_axis(
+            func1d=joint_entropy, axis=0, arr=C, y=y)
+
+        return joint_entropy
 
     @classmethod
     def ft_mut_inf(cls, C: np.ndarray, y: np.ndarray) -> np.ndarray:
@@ -109,7 +126,7 @@ class MFEInfoTheory:
 
     @classmethod
     def ft_ns_ratio(cls, C: np.ndarray, y: np.ndarray) -> np.ndarray:
-        """To do this doc."""
+        """Compute noisiness of attributes."""
         ent_attr = sum(MFEInfoTheory.ft_attr_ent(C))
         mutual_info = sum(MFEInfoTheory.ft_mut_inf(C, y))
 
@@ -120,6 +137,6 @@ if __name__ == "__main__":
     from sklearn import datasets
     iris = datasets.load_iris()
 
-    res = MFEInfoTheory.ft_attr_conc(iris.data)
+    res = MFEInfoTheory.ft_joint_entropy(iris.data, iris.target)
 
     print(res)
