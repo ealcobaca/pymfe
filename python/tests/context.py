57277e0a94a9485d92ece7015c9a75162424d8ec
FelSiq <felipe.siqueira@usp.br>
2019-01-26 17:12:12 -0200

    Improved test structure and infotheo tests

--- python/tests/context.py
+++ python/tests/context.py
@@ -16,6 +16,12 @@ import os
 import sys
 
 import pandas as pd
+import numpy as np
+import rpy2.robjects
+import rpy2.rinterface
+import rpy2.robjects.packages
+import rpy2.robjects.numpy2ri
+import rpy2.robjects.pandas2ri
 
 IMPORT_PATH_TUPLE = ("..", "../pymfe", "../../rwrapper/pymfe/mfe/")
 
@@ -24,8 +30,12 @@ for new_path in IMPORT_PATH_TUPLE:
         0, os.path.abspath(os.path.join(os.path.dirname(__file__), new_path)))
 
 import pymfe  # noqa: E402, F401
+from pymfe.mfe import MFE  # noqa: E402
 
 TARGET_COL_NAMES = ("target", )
+
+EPSILON = 1.0e-6
+"""Small value to assert floating point correctness w/o numeric errors."""
 
 
 def _get_test_data(path):
@@ -33,7 +43,7 @@ def _get_test_data(path):
     test_dataset_list = os.listdir(path)
 
     test_data = []
-    for dataset_name in test_dataset_list:
+    for dataset_name in sorted(test_dataset_list):
         if dataset_name.endswith(".csv"):
             test_data.append(pd.read_csv(
                 path + dataset_name,
@@ -45,3 +55,63 @@ def _get_test_data(path):
 
 TEST_PATH = "./test_datasets/"
 DATASET_LIST = _get_test_data(TEST_PATH)
+
+
+def get_col_indexes(dataset):
+    """Split columns of independent and target attributes.
+
+    It is assumed that the target attribute of test datasets
+    is in ``TARGET_COL_NAME``.
+    """
+    ind_targ = np.isin(dataset.columns, TARGET_COL_NAMES)
+    ind_attr = ~ind_targ
+
+    return ind_targ, ind_attr
+
+
+def get_val_py(dataset, feat_name_py, ind_attr, ind_targ, method_args,
+               fit_args, summary_name):
+    """Instantiate, Fit and Extract summarized metafeature from MFE."""
+    X = dataset.iloc[:, ind_attr].values
+    y = dataset.iloc[:, ind_targ].values
+
+    _, res_mfe_py_vals = MFE(
+        features=feat_name_py,
+        summary=summary_name,
+    ).fit(
+        X=X, y=y, **fit_args).extract(**method_args)
+
+    return res_mfe_py_vals
+
+
+def na_to_nan(values):
+    """Convert any R ``NA`` object to :obj:`np.nan` within a list."""
+    return np.array([
+        np.nan if isinstance(x, rpy2.rinterface.NALogicalType) else x
+        for x in list(values)
+    ])
+
+
+def get_val_r(dataset, feat_name_r, ind_attr, ind_targ, summary_name,
+              group_name):
+    """Get summarized metafeature value from R mfe implementation."""
+    rpy2.robjects.pandas2ri.activate()
+    rpy2.robjects.numpy2ri.activate()
+
+    dataset_r = rpy2.robjects.pandas2ri.py2ri(dataset)
+
+    mfe = rpy2.robjects.packages.importr("mfe")
+
+    method_to_call = getattr(mfe, group_name)
+
+    res_mfe_r = method_to_call(
+        x=dataset_r.rx(ind_attr),
+        y=dataset_r.rx(ind_targ),
+        features=feat_name_r,
+        summary=summary_name)
+
+    rpy2.robjects.pandas2ri.deactivate()
+
+    res_mfe_r_vals = na_to_nan(res_mfe_r.rx2(feat_name_r))
+
+    return res_mfe_r_vals
--- python/tests/test_general.py
+++ python/tests/test_general.py
@@ -2,79 +2,12 @@
 import pytest
 
 import numpy as np
-import rpy2.robjects
-import rpy2.rinterface
-import rpy2.robjects.packages
-import rpy2.robjects.numpy2ri
-import rpy2.robjects.pandas2ri
 
 import context
-from pymfe.mfe import MFE
 
 
 class TestGeneral:
     """TestClass dedicated to test General metafeatures."""
-
-    EPSILON = 1.0e-6
-    """Small value to assert floating point correctness w/o numeric errors."""
-
-    @classmethod
-    def _get_col_indexes(cls, dataset):
-        """Split columns of independent and target attributes.
-
-        It is assumed that the target attribute of test datasets
-        is in ``context.TARGET_COL_NAME``.
-        """
-        ind_targ = np.isin(dataset.columns, context.TARGET_COL_NAMES)
-        ind_attr = ~ind_targ
-
-        return ind_targ, ind_attr
-
-    @classmethod
-    def _get_val_py(cls, dataset, feat_name_py, ind_attr, ind_targ,
-                    method_args, fit_args, summary_name):
-        """Instantiate, Fit and Extract summarized metafeature from MFE."""
-        X = dataset.iloc[:, ind_attr].values
-        y = dataset.iloc[:, ind_targ].values
-
-        _, res_mfe_py_vals = MFE(
-            features=feat_name_py,
-            summary=summary_name,
-        ).fit(
-            X=X, y=y, **fit_args).extract(**method_args)
-
-        return res_mfe_py_vals
-
-    @classmethod
-    def _na_to_nan(cls, values):
-        """Convert any R ``NA`` object to :obj:`np.nan` within a list."""
-        return np.array([
-            np.nan if isinstance(x, rpy2.rinterface.NALogicalType) else x
-            for x in list(values)
-        ])
-
-    @classmethod
-    def _get_val_r(cls, dataset, feat_name_r, ind_attr, ind_targ,
-                   summary_name):
-        """Get summarized metafeature value from R mfe implementation."""
-        rpy2.robjects.pandas2ri.activate()
-        rpy2.robjects.numpy2ri.activate()
-
-        dataset_r = rpy2.robjects.pandas2ri.py2ri(dataset)
-
-        mfe = rpy2.robjects.packages.importr("mfe")
-
-        res_mfe_r = mfe.general(
-            x=dataset_r.rx(ind_attr),
-            y=dataset_r.rx(ind_targ),
-            features=feat_name_r,
-            summary=summary_name)
-
-        rpy2.robjects.pandas2ri.deactivate()
-
-        res_mfe_r_vals = TestGeneral._na_to_nan(res_mfe_r.rx2(feat_name_r))
-
-        return res_mfe_r_vals
 
     @pytest.mark.parametrize(
         ("dt_id, fit_args, feat_name_py,"
@@ -134,19 +67,19 @@ class TestGeneral:
             (1, {}, "num_to_cat", {}, "numToCat", "all"),
         ),
     )
-    def test_ft_attr_to_inst(self, dt_id, fit_args, feat_name_py, method_args,
-                             feat_name_r, summary_name):
+    def test_ft_methods_general(self, dt_id, fit_args, feat_name_py,
+                                method_args, feat_name_r, summary_name):
         """Compare metafeat. values against correspondent from R mfe."""
         dataset = context.DATASET_LIST[dt_id]
 
-        ind_targ, ind_attr = TestGeneral._get_col_indexes(dataset)
+        ind_targ, ind_attr = context.get_col_indexes(dataset)
 
-        res_mfe_py = TestGeneral._get_val_py(dataset, feat_name_py, ind_attr,
-                                             ind_targ, method_args, fit_args,
-                                             summary_name)
+        res_mfe_py = context.get_val_py(dataset, feat_name_py, ind_attr,
+                                        ind_targ, method_args, fit_args,
+                                        summary_name)
 
-        res_mfe_r = TestGeneral._get_val_r(dataset, feat_name_r, ind_attr,
-                                           ind_targ, summary_name)
+        res_mfe_r = context.get_val_r(dataset, feat_name_r, ind_attr, ind_targ,
+                                      summary_name, "general")
 
         assert (all(np.isnan(res_mfe_py) == np.isnan(res_mfe_r))
-                or all(res_mfe_py - res_mfe_r < TestGeneral.EPSILON))
+                or all(res_mfe_py - res_mfe_r < context.EPSILON))
