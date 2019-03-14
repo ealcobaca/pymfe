9aca3b40d152e2d2711ea3241a2ab949f0bafb9c
lpfgarcia <lpfgarcia@gmail.com>
2019-03-14 18:54:42 -0300

    rename functions

--- python/pymfe/model_based.py
+++ python/pymfe/model_based.py
@@ -16,26 +16,24 @@ def dt(X, y):
 
 def extract(model, X, y):
 
-    leaf = model.apply(X)
-    node = model.tree_.node_count
-
-    table = np.zeros((node, 4))
+    table = np.zeros((model.tree_.node_count, 4))
     table[:,0] = model.tree_.feature
     table[:,2] = model.tree_.n_node_samples
 
-    tmp = np.array([leaf, y])
+    leaves = model.apply(X)
+    tmp = np.array([leaves, y + 1])
 
-    for x in set(leaf):
-        table[x,3] = Counter(tmp[1,tmp[0,:] == x]).keys()[0] + 1
+    for x in set(leaves):
+        table[x,3] = Counter(tmp[1,tmp[0,:] == x]).keys()[0]
         table[x,1] = 1
 
     return table
 
-def _leaves(model, X, y):
+def ft_leaves(model, X, y):
     aux = extract(model, X, y)
-    return aux.sum(axis=0)[1]
+    return sum(aux[:,1])
 
-def _treeDepth(tree):
+def ft_treeDepth(model, X, y):
 
     def nodeDepth(node, depth, l, r, depths):
         depths += [depth]
@@ -44,51 +42,51 @@ def _treeDepth(tree):
             nodeDepth(r[node], depth + 1, l, r, depths)
 
     depths = []
-    nodeDepth(0, 0, tree.children_left, tree.children_right, depths)
+    nodeDepth(0, 0, model.tree_.children_left, 
+        model.tree_.children_right, depths)
     return np.array(depths)
 
-def _leavesBranch(model, X, y):
+def ft_leavesBranch(model, X, y):
     return _treeDepth(model.tree_)[extract(model, X, y)[:,1] == 1]
 
-def _leavesCorrob(model, X, y):
+def ft_leavesCorrob(model, X, y):
     aux = extract(model, X, y)
     return aux[:,2][aux[:,1] == 1]/len(X)
 
-def _treeShape(model, X, y):
+def ft_treeShape(model, X, y):
     aux = _treeDepth(model.tree_)[extract(model, X, y)[:,1] == 1]
     return np.log2(aux)
 
-def _leavesHomo(model, X, y):
+def ft_leavesHomo(model, X, y):
     return _leaves(model, X, y)/_treeShape(model, X, y)
 
-def _leavesPerClass(model, X, y):
+def ft_leavesPerClass(model, X, y):
     aux = Counter(extract(model, X, y)[:,3]).items()
     aux = aux/_leaves(model, X, y)
     return aux[1:,1]
 
-def _nodes(model, X, y):
+def ft_nodes(model, X, y):
     return sum(extract(model, X, y)[:,1] != 1)
 
-def _nodesPerAttr(model, X, y):
+def ft_nodesPerAttr(model, X, y):
     return _nodes(model, X, y)/X.shape[1]
 
-def _nodesPerInst(model, X, y):
-    return _nodes(model, X, y)/len(X)
+def ft_nodesPerInst(model, X, y):
+    return float(_nodes(model, X, y))/len(X)
 
-def _nodesPerLevel(model, X, y):
+def ft_nodesPerLevel(model, X, y):
     aux = _treeDepth(model.tree_)[extract(model, X, y)[:,1] == 0]
-    aux = np.array(Counter(aux).items())[:,1]
-    return aux/_leaves(model, X, y)
+    return np.array(Counter(aux).items())[:,1]
 
-def _nodesRepeated(model, X, y):
+def ft_nodesRepeated(model, X, y):
     aux = extract(model, X, y)[:,0] 
     aux = aux[aux > 0]
     aux = np.array(Counter(aux).items())[:,1]
     return aux
 
-def _treeImbalance(model, X, y):
+def ft_treeImbalance(model, X, y):
     pass
 
-def _varImportance(model, X, y):
+def ft_varImportance(model, X, y):
     return model.tree_.compute_feature_importances()
 
