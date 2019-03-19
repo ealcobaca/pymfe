43e8be6a757c53da614948170ebfede8cfbbb827
e.alcobaca <e.alcobaca@gmail.com>
2019-03-19 15:17:07 -0300

    7 landmarking metafeatures added

--- python/pymfe/landmarking.py
+++ python/pymfe/landmarking.py
@@ -9,7 +9,9 @@ import typing as t
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import cross_val_score
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 import numpy as np
 
 
@@ -25,50 +27,131 @@ class MFELandmarking:
 
         if X is not None and y is not None\
            and not {"model", "table"}.issubset(kwargs):
-            skf = StratifiedKFold(n_splits=2)
+            skf = StratifiedKFold(n_splits=10)
             prepcomp_vals["skf"] = skf
+            prepcomp_vals["random_state"] = 0
+            prepcomp_vals["metric"] = accuracy_score
 
         return prepcomp_vals
 
     @classmethod
-    def ft_best_node(cls, X, y, skf):
-        clf = DecisionTreeClassifier(max_depth=1)
-        result = cross_val_score(clf, X, y,
-                                 scoring="accuracy", cv=10)
-        # result = []
-        # for train_index, test_index in skf.split(X, y):
-        #     model = DecisionTreeClassifier(max_depth=1)
-        #     model.fit(X[train_index], y[train_index])
-        #     pred = model.predict(X[test_index])
-        #     result.append(accuracy_score(y[test_index], pred))
+    def importance(cls, X, y, random_state):
+        clf = DecisionTreeClassifier(random_state=random_state).fit(X, y)
+        return np.argsort(clf.feature_importances_)
+
+    @classmethod
+    def ft_best_node(cls, X, y, skf, metric, random_state):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            importance = MFELandmarking.importance(X[train_index],
+                                                   y[train_index],
+                                                   random_state)
+            model = DecisionTreeClassifier(max_depth=1)
+            X_train = X[train_index, :][:, [importance[-1]]]
+            X_test = X[test_index, :][:, [importance[-1]]]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
 
         return np.array(result)
 
     @classmethod
-    def ft_elite_nn(cls):
-        pass
+    def ft_random_node(cls, X, y, skf, metric, random_state):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            attr = np.random.randint(0, X.shape[1], size=(1,))
+            model = DecisionTreeClassifier(max_depth=1,
+                                           random_state=random_state)
+            X_train = X[train_index, :][:, attr]
+            X_test = X[test_index, :][:, attr]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
-    def ft_linear_discr(cls):
-        pass
+    def ft_worst_node(cls, X, y, skf, metric, random_state):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            importance = MFELandmarking.importance(X[train_index],
+                                                   y[train_index],
+                                                   random_state)
+            model = DecisionTreeClassifier(max_depth=1,
+                                           random_state=random_state)
+            X_train = X[train_index, :][:, [importance[0]]]
+            X_test = X[test_index, :][:, [importance[0]]]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
-    def ft_naive_bayes(cls):
-        pass
+    def ft_elite_nn(cls, X, y, skf, metric, random_state):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            importance = MFELandmarking.importance(X[train_index],
+                                                   y[train_index],
+                                                   random_state)
+            model = KNeighborsClassifier(n_neighbors=1)
+            X_train = X[train_index, :][:, [importance[-1]]]
+            X_test = X[test_index, :][:, [importance[-1]]]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
-    def ft_one_nn(cls):
-        pass
+    def ft_linear_discr(cls, X, y, skf, metric):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = LinearDiscriminantAnalysis()
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
-    def ft_random_node(cls):
-        pass
+    def ft_naive_bayes(cls, X, y, skf, metric):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = GaussianNB()
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
-    def worst_node(cls):
-        pass
+    def ft_one_nn(cls, X, y, skf, metric):
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = KNeighborsClassifier(n_neighbors=1)
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
 
-    @classmethod
-    def ft_test_landmarking(cls):
-        """To do."""
-        return 0.0
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(metric(y_test, pred))
+
+        return np.array(result)
