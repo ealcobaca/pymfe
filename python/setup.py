bfe232fdae98abbfa2d9cb1261fc31b3095360c3
e.alcobaca <e.alcobaca@gmail.com>
2019-03-19 20:04:51 -0300

    setup updated

--- python/setup.py
+++ python/setup.py
@@ -1,5 +1,12 @@
 """Setup for pymfe package."""
 import setuptools
+import os
+
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(my_path, 'requirements.txt')) as fp:
+    install_reqs = [r.rstrip() for r in fp.readlines()]
+    print(install_reqs)
 
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
@@ -8,12 +15,13 @@ setuptools.setup(
     name="pymfe",
     version="0.0.1",
     author="Edesio Alcobaça, Felipe Alves Siqueira",
-    author_email="[email?], felipe.siqueira@usp.br",
+    author_email="edesio@usp.br, felipe.siqueira@usp.br",
     description="Metafeature extractor",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
     url="https://github.com/ealcobaca/pyMFE",
     packages=setuptools.find_packages(),
+    install_requires=install_reqs,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
