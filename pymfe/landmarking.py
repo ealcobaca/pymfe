5c1ecea10627df26019afc8acfdc3cebeed98087
e.alcobaca <e.alcobaca@gmail.com>
2019-03-21 13:37:08 -0300

    Docstring added

--- pymfe/landmarking.py
+++ pymfe/landmarking.py
@@ -21,10 +21,32 @@ class MFELandmarking:
     def precompute_landmarking_class(cls, X: np.ndarray, y: np.ndarray,
                                      folds: int, random_state: t.Optional[int],
                                      **kwargs) -> t.Dict[str, t.Any]:
+        """Precompute distinct classes and its frequencies from ``y``.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+            folds (int): number of folds to k-fold cross validation.
+            random_state (int, optional): If int, random_state is the seed used
+            by the random number generator; If RandomState instance, random_st-
+            ate is the random number generator; If None, the random number gen-
+            erator is the RandomState instance used by np.random.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            dict: with following precomputed items:
+                - ``skf`` (:obj:`StratifiedKFold`): Stratified K-Folds cross-
+                validator. Provides train/test indices to split data in
+                train/test sets.
+        """
+
         prepcomp_vals = {}
 
         if X is not None and y is not None\
-           and not {"model", "table"}.issubset(kwargs):
+           and not {"skf"}.issubset(kwargs):
             skf = StratifiedKFold(n_splits=folds, random_state=random_state)
             prepcomp_vals["skf"] = skf
 
@@ -40,6 +62,32 @@ class MFELandmarking:
     def ft_best_node(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
                      score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
                      random_state: t.Optional[int]) -> np.ndarray:
+        """Construct a single decision tree node model induced by the most
+        informative attribute to establish the linear separability.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            random_state (int, optional): If int, random_state is the seed used
+                by the random number generator; If RandomState instance, ran-
+                dom_state is the random number generator; If None, the random
+                number generator is the RandomState instance used by np.random.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
         result = []
         for train_index, test_index in skf.split(X, y):
             model = DecisionTreeClassifier(max_depth=1,
@@ -52,12 +100,38 @@ class MFELandmarking:
             pred = model.predict(X_test)
             result.append(score(y_test, pred))
 
-        return np.ndarray(result)
+        return np.array(result)
 
     @classmethod
     def ft_random_node(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
                        score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
                        random_state: t.Optional[int]) -> np.ndarray:
+        """Construct a single decision tree node model induced by a random
+        attribute.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            random_state (int, optional): If int, random_state is the seed used
+                by the random number generator; If RandomState instance, ran-
+                dom_state is the random number generator; If None, the random
+                number generator is the RandomState instance used by np.random.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
         result = []
         for train_index, test_index in skf.split(X, y):
             attr = np.random.randint(0, X.shape[1], size=(1,))
@@ -71,12 +145,38 @@ class MFELandmarking:
             pred = model.predict(X_test)
             result.append(score(y_test, pred))
 
-        return np.ndarray(result)
+        return np.array(result)
 
     @classmethod
     def ft_worst_node(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
                       score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
                       random_state: t.Optional[int]) -> np.ndarray:
+        """Construct a single decision tree node model induced by the worst
+        informative attribute.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            random_state (int, optional): If int, random_state is the seed used
+                by the random number generator; If RandomState instance, ran-
+                dom_state is the random number generator; If None, the random
+                number generator is the RandomState instance used by np.random.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
         result = []
         for train_index, test_index in skf.split(X, y):
             importance = MFELandmarking.importance(X[train_index],
@@ -92,12 +192,159 @@ class MFELandmarking:
             pred = model.predict(X_test)
             result.append(score(y_test, pred))
 
-        return np.ndarray(result)
+        return np.array(result)
+
+    @classmethod
+    def ft_linear_discr(cls, X: np.ndarray, y: np.ndarray,
+                        skf: StratifiedKFold,
+                        score: t.Callable[[np.ndarray, np.ndarray], np.ndarray]
+                        ) -> np.ndarray:
+        """Apply the Linear Discriminant classifier to construct a linear split
+        (non parallel axis) in the data to establish the linear separability.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
+
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = LinearDiscriminantAnalysis()
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(score(y_test, pred))
+
+        return np.array(result)
+
+    @classmethod
+    def ft_naive_bayes(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
+                       score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
+                       ) -> np.ndarray:
+        """Evaluate the performance of the Naive Bayes classifier. It assumes
+        that the attributes are independent and each example belongs to a cer-
+        tain class based on the Bayes probability.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
+
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = GaussianNB()
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(score(y_test, pred))
+
+        return np.array(result)
+
+    @classmethod
+    def ft_one_nn(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
+                  score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
+                  ) -> np.ndarray:
+        """Evaluate the performance of the 1-nearest neighbor classifier. It
+        uses the euclidean distance of the nearest neighbor to determine how
+        noisy is the data.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
+
+        result = []
+        for train_index, test_index in skf.split(X, y):
+            model = KNeighborsClassifier(n_neighbors=1)
+            X_train = X[train_index, :]
+            X_test = X[test_index, :]
+            y_train, y_test = y[train_index], y[test_index]
+
+            model.fit(X_train, y_train)
+            pred = model.predict(X_test)
+            result.append(score(y_test, pred))
+
+        return np.array(result)
 
     @classmethod
     def ft_elite_nn(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
                     score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
                     random_state: t.Optional[int]) -> np.ndarray:
+        """Elite nearest neighbor uses the most informative attribute in the
+        dataset to induce the 1-nearest neighbor. With the subset of informati-
+        ve attributes is expected that the models should be noise tolerant.
+
+        Args:
+            X (:obj:`np.ndarray`, optional): attributes from fitted data.
+
+            y (:obj:`np.ndarray`, optional): target attribute from fitted data.
+
+            skf (:obj:`StratifiedKFold`): stratified K-Folds cross-validator.
+                Provides train/test indices to split data in train/test sets.
+
+            score (callable): function to compute score of the K-fold evalua-
+                tions. Possible functions are described in `scoring.py` module.
+
+            random_state (int, optional): If int, random_state is the seed used
+                by the random number generator; If RandomState instance, ran-
+                dom_state is the random number generator; If None, the random
+                number generator is the RandomState instance used by np.random.
+
+            **kwargs: additional arguments. May have previously precomputed be-
+                fore this method from other precomputed methods, so they can
+                help speed up this precomputation.
+
+        Return:
+            np.ndarray: The performance of each fold.
+        """
         result = []
         for train_index, test_index in skf.split(X, y):
             importance = MFELandmarking.importance(X[train_index],
@@ -112,58 +359,4 @@ class MFELandmarking:
             pred = model.predict(X_test)
             result.append(score(y_test, pred))
 
-        return np.ndarray(result)
-
-    @classmethod
-    def ft_linear_discr(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
-                        score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
-                        ) -> np.ndarray:
-
-        result = []
-        for train_index, test_index in skf.split(X, y):
-            model = LinearDiscriminantAnalysis()
-            X_train = X[train_index, :]
-            X_test = X[test_index, :]
-            y_train, y_test = y[train_index], y[test_index]
-
-            model.fit(X_train, y_train)
-            pred = model.predict(X_test)
-            result.append(score(y_test, pred))
-
-        return np.ndarray(result)
-
-    @classmethod
-    def ft_naive_bayes(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
-                       score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
-                       ) -> np.ndarray:
-
-        result = []
-        for train_index, test_index in skf.split(X, y):
-            model = GaussianNB()
-            X_train = X[train_index, :]
-            X_test = X[test_index, :]
-            y_train, y_test = y[train_index], y[test_index]
-
-            model.fit(X_train, y_train)
-            pred = model.predict(X_test)
-            result.append(score(y_test, pred))
-
-        return np.ndarray(result)
-
-    @classmethod
-    def ft_one_nn(cls, X: np.ndarray, y: np.ndarray, skf: StratifiedKFold,
-                  score: t.Callable[[np.ndarray, np.ndarray], np.ndarray],
-                  ) -> np.ndarray:
-
-        result = []
-        for train_index, test_index in skf.split(X, y):
-            model = KNeighborsClassifier(n_neighbors=1)
-            X_train = X[train_index, :]
-            X_test = X[test_index, :]
-            y_train, y_test = y[train_index], y[test_index]
-
-            model.fit(X_train, y_train)
-            pred = model.predict(X_test)
-            result.append(score(y_test, pred))
-
-        return np.ndarray(result)
+        return np.array(result)
