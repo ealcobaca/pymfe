6717480b4e7df9f2c0a15a441aa25f3590166030
e.alcobaca <e.alcobaca@gmail.com>
2019-03-25 15:38:40 -0300

    tests to general meta-features group

--- tests/utils.py
+++ tests/utils.py
@@ -8,16 +8,26 @@ import pandas as pd
 
 
 DATA_ID = [
-    "tests/test_datasets/mix_aids.arff"
+    "tests/test_datasets/mix_aids.arff",
+    "tests/test_datasets/cat_kr-vs-kp.arff",
+    "tests/test_datasets/num_Iris.arff"
+]
+
+DATA_ = [
+    None,
+    None,
+    None
 ]
 
 
 def load_xy(dt_id):
     """Returns a dataset loaded from arff file.
     """
-    data = arff.load(open(DATA_ID[dt_id], 'r'))
-    df = pd.DataFrame(data['data'])
-    y = df.iloc[:, -1]
-    X = df.iloc[:, :df.shape[1]-1]
+    if DATA_[dt_id] is None:
+        data = arff.load(open(DATA_ID[dt_id], 'r'))
+        df = pd.DataFrame(data['data'])
+        y = df.iloc[:, -1]
+        X = df.iloc[:, :df.shape[1]-1]
+        DATA_[dt_id] = (X, y)
 
-    return (X, y)
+    return DATA_[dt_id]
