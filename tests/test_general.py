78ca0a2d085c7a6a70c00c60a5b0f819c07eff85
FelSiq <felipe.siqueira@usp.br>
2019-03-26 10:21:50 -0300

    Updated test_general.py after fixing some glitches

--- tests/test_general.py
+++ tests/test_general.py
@@ -15,21 +15,21 @@ class TestGeneral():
         # Mixed data
         ###################
         (0, 'attr_to_inst', 0.08),
-        # (0, 'cat_to_num', 1),
+        (0, 'cat_to_num', 1),
         (0, 'freq_class', [0.50, 0.0]),
         (0, 'inst_to_attr', 12.50),
         (0, 'nr_attr', 4),
         (0, 'nr_bin', 0),
-        # (0, 'nr_cat', 2),
+        (0, 'nr_cat', 2),
         (0, 'nr_class', 2),
         (0, 'nr_inst', 50),
-        # (0, 'nr_num', 11),
-        # (0, 'num_to_cat', 2.75)
+        (0, 'nr_num', 2),
+        (0, 'num_to_cat', 1.0),
         ###################
         # Categorical data
         ###################
         (1, 'attr_to_inst', 36/3196),
-        # (1, 'cat_to_num', np.nan),
+        (1, 'cat_to_num', np.nan),
         (1, 'freq_class', [0.5, 0.03141713]),
         (1, 'inst_to_attr', 88.77778),
         (1, 'nr_attr', 36),
@@ -37,22 +37,22 @@ class TestGeneral():
         (1, 'nr_cat', 36),
         (1, 'nr_class', 2),
         (1, 'nr_inst', 3196),
-        # (1, 'nr_num', 0),
-        # (1, 'num_to_cat', 0)
+        (1, 'nr_num', 0),
+        (1, 'num_to_cat', 0),
         ###################
-        # Categorical data
+        # Numerical data
         ###################
         (2, 'attr_to_inst', 0.02666667),
-        # (0, 'cat_to_num', 0.0),
+        (2, 'cat_to_num', 0.0),
         (2, 'freq_class', [0.33333333, 0.0]),
         (2, 'inst_to_attr', 37.50),
         (2, 'nr_attr', 4),
         (2, 'nr_bin', 0),
-        # (0, 'nr_cat', 0),
+        (2, 'nr_cat', 0),
         (2, 'nr_class', 3),
         (2, 'nr_inst', 150),
         (2, 'nr_num', 4),
-        # (0, 'num_to_cat', np.nan)
+        (2, 'num_to_cat', np.nan),
     ])
     def test_ft_methods_general(self, dt_id, ft_name, exp_value):
         """Function to test each meta-feature belongs to general group.
@@ -60,9 +60,10 @@ class TestGeneral():
         X, y = load_xy(dt_id)
         mfe = MFE(groups=["general"],
                   features=[ft_name]).fit(X.values, y.values)
-        value = mfe.extract()
+        value = mfe.extract()[1]
 
         if exp_value is np.nan:
-            assert value[1] is exp_value
+            assert value[0] is exp_value
+
         else:
-            assert np.allclose(value[1], exp_value)
+            assert np.allclose(value, exp_value)
