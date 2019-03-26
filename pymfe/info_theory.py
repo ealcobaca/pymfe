7d3f4198e909e6eac720fc1d3e3277979c91f858
FelSiq <felipe.siqueira@usp.br>
2019-03-26 10:19:05 -0300

    Small improvement at class docs

--- pymfe/info_theory.py
+++ pymfe/info_theory.py
@@ -25,21 +25,27 @@ class MFEInfoTheory:
     prefix is predefined within ``_internal`` module.
 
     All method signature follows the conventions and restrictions listed below:
-    1. For independent attribute data, ``X`` means ``every type of attribu-
-        te``, ``N`` means ``Numeric attributes only`` and ``C`` stands for
-        ``Categorical attributes only``.
+        1. For independent attribute data, ``X`` means ``every type of attribu-
+            te``, ``N`` means ``Numeric attributes only`` and ``C`` stands for
+            ``Categorical attributes only``. It is important to note that the
+            categorical attribute sets between ``X`` and ``C`` and the numeri-
+            cal attribute sets between ``X`` and ``N`` may differ due to data
+            transformations, performed while fitting data into MFE model, en-
+            abled by, respectively, ``transform_num`` and ``transform_cat``
+            arguments from ``fit`` (MFE method).
 
-    2. Only ``X``, ``y``, ``N``, ``C`` and ``splits`` are allowed to be re-
-        quired method arguments. All other arguments must be strictly opti-
-        onal (i.e., has a predefined default value).
+        2. Only arguments in MFE ``_custom_args_ft`` attribute (set up inside
+            ``fit`` method) are allowed to be required method arguments. All
+            other arguments must be strictly optional (i.e., has a predefined
+            default value).
 
-    3. The initial assumption is that the user can change any optional ar-
-        gument, without any previous verification of argument value or its
-        type, via **kwargs argument of ``extract`` method of MFE class.
+        3. The initial assumption is that the user can change any optional ar-
+            gument, without any previous verification of argument value or its
+            type, via **kwargs argument of ``extract`` method of MFE class.
 
-    4. The return value of all feature extraction methods should be a sin-
-        gle value or a generic Sequence (preferably a :obj:`np.ndarray`)
-        type with numeric values.
+        4. The return value of all feature extraction methods should be a sin-
+            gle value or a generic Sequence (preferably a :obj:`np.ndarray`)
+            type with numeric values.
 
     There is another type of method adopted for automatic detection. It is ad-
     opted the prefix ``precompute_`` for automatic detection of these methods.
