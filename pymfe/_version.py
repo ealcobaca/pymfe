e85cd77a469b341664817c55976649c36d4756b0
e.alcobaca <e.alcobaca@gmail.com>
2019-04-23 21:56:19 -0300

    the version file

---
+++ pymfe/_version.py
@@ -0,0 +1,25 @@
+"""
+``pymfe`` is a meta-feature extractor to deal with meta-learning problems
+in machine learning and AutoML.
+"""
+# Based on NiLearn package
+# License: simplified BSD
+#
+# PEP0440 compatible formatted version, see:
+# https://www.python.org/dev/peps/pep-0440/
+#
+# Generic release markers:
+# X.Y
+# X.Y.Z # For bugfix releases
+#
+# Admissible pre-release markers:
+# X.YaN # Alpha release
+# X.YbN # Beta release
+# X.YrcN # Release Candidate
+# X.Y # Final release
+#
+# Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
+# 'X.Y.dev0' is the canonical version of 'X.Y.dev'
+#
+
+__version__ = '0.0.2'
