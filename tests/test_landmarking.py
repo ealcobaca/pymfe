82732ccec12a056897cbbd69e5f2bb0c89559409
FelSiq <felipe.siqueira@usp.br>
2019-05-17 16:44:44 -0300

    Added relative landmarking tests

--- tests/test_landmarking.py
+++ tests/test_landmarking.py
@@ -105,3 +105,64 @@ class TestLandmarking():
 
         else:
             assert np.allclose(value, exp_value)
+
+    @pytest.mark.parametrize(
+        "dt_id, summary, precompute, sample_size, exp_value",
+        [
+            #######################################
+            # Mean Relative Landmarking
+            #######################################
+            (0, "mean", False, 1.0, [5.0, 4.0, 7.0, 6.0, 3.0, 1.5, 1.5]),
+            (0, "mean", True,  1.0, [5.0, 4.0, 7.0, 6.0, 3.0, 1.5, 1.5]),
+            (1, "mean", False, 1.0, [2.0, 3.0, 7.0, 5.0, 6.0, 4.0, 1.0]),
+            (1, "mean", True,  1.0, [2.0, 3.0, 7.0, 5.0, 6.0, 4.0, 1.0]),
+            (2, "mean", False, 1.0, [2.5, 4.0, 7.0, 5.0, 6.0, 2.5, 1.0]),
+            (2, "mean", True,  1.0, [2.5, 4.0, 7.0, 5.0, 6.0, 2.5, 1.0]),
+
+            #######################################
+            # Mean Relative Subsampling Landmarking
+            #######################################
+            (0, "mean", False, 0.5, [5.0, 7.0, 2.0, 6.0, 1.0, 4.0, 3.0]),
+            (0, "mean", True,  0.5, [5.0, 7.0, 2.0, 6.0, 1.0, 4.0, 3.0]),
+            (1, "mean", False, 0.5, [5.0, 3.0, 7.0, 4.0, 6.0, 2.0, 1.0]),
+            (1, "mean", True,  0.5, [5.0, 3.0, 7.0, 4.0, 6.0, 2.0, 1.0]),
+            (2, "mean", False, 0.5, [2.5, 4.0, 6.0, 5.0, 7.0, 2.5, 1.0]),
+            (2, "mean", True,  0.5, [2.5, 4.0, 6.0, 5.0, 7.0, 2.5, 1.0]),
+
+            #######################################
+            # Std Relative Landmarking
+            #######################################
+            (0, "sd", False, 1.0, [4.0, 5.0, 7.0, 6.0, 3.0, 1.5, 1.5]),
+            (0, "sd", True,  1.0, [4.0, 5.0, 7.0, 6.0, 3.0, 1.5, 1.5]),
+            (1, "sd", False, 1.0, [6.0, 3.0, 2.0, 4.0, 1.0, 5.0, 7.0]),
+            (1, "sd", True,  1.0, [6.0, 3.0, 2.0, 4.0, 1.0, 5.0, 7.0]),
+            (2, "sd", False, 1.0, [1.5, 6.0, 3.5, 3.5, 5.0, 1.5, 7.0]),
+            (2, "sd", True,  1.0, [1.5, 6.0, 3.5, 3.5, 5.0, 1.5, 7.0]),
+
+            #######################################
+            # Std Relative Subsampling Landmarking
+            #######################################
+            (0, "sd", False, 0.5, [6.0, 5.0, 4.0, 7.0, 3.0, 1.0, 2.0]),
+            (0, "sd", True,  0.5, [6.0, 5.0, 4.0, 7.0, 3.0, 1.0, 2.0]),
+            (1, "sd", False, 0.5, [3.0, 7.0, 1.0, 4.0, 2.0, 6.0, 5.0]),
+            (1, "sd", True,  0.5, [3.0, 7.0, 1.0, 4.0, 2.0, 6.0, 5.0]),
+            (2, "sd", False, 0.5, [2.5, 6.0, 5.0, 7.0, 1.0, 2.5, 4.0]),
+            (2, "sd", True,  0.5, [2.5, 6.0, 5.0, 7.0, 1.0, 2.5, 4.0]),
+        ])
+    def test_ft_method_relative(self, dt_id, summary, precompute,
+                                sample_size, exp_value):
+        """."""
+        precomp_group = "relative" if precompute else None
+
+        X, y = load_xy(dt_id)
+        mfe = MFE(
+            groups=["relative"],
+            summary=summary,
+            sample_size=sample_size,
+            random_state=1234)
+
+        mfe.fit(X.values, y.values, precomp_groups=precomp_group)
+
+        _, vals = mfe.extract()
+
+        assert np.allclose(vals, exp_value)
