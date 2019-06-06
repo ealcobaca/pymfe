b9c79a8e377d2a41c8ed97d6a4d78742fee9ae56
lpfgarcia <lpfgarcia@gmail.com>
2019-06-06 14:41:11 -0300

    update branch + clustering metafeatures

--- setup.py
+++ setup.py
@@ -48,7 +48,8 @@ CLASSIFIERS = ['Intended Audience :: Science/Research',
                'Programming Language :: Python :: 3.7']
 
 
-INSTALL_REQUIRES = ['numpy', 'scipy', 'sklearn', 'patsy', 'pandas']
+INSTALL_REQUIRES = ['numpy', 'scipy', 'sklearn', 'patsy', 'pandas', 
+  'statsmodels']
 
 
 EXTRAS_REQUIRE = {
