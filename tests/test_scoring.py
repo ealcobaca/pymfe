e50aecda866e43ace3eda1d006c2aec79009e332
e.alcobaca <e.alcobaca@gmail.com>
2019-08-22 16:29:54 -0300

    test scoring fixed

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -2,58 +2,60 @@
 
 import pytest
 import numpy as np
-import sklearn.metrics
 
+from pymfe.scoring import accuracy
+from pymfe.scoring import balanced_accuracy
+from pymfe.scoring import f1
 
 def test_accuracy():
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 1.0
+    assert accuracy(y_true, y_pred) == 1.0
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 0.0
+    assert accuracy(y_true, y_pred) == 0.0
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 0.5
+    assert accuracy(y_true, y_pred) == 0.5
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 0, 0, 1, 1, 0, 0, 0])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 0.6
+    assert accuracy(y_true, y_pred) == 0.6
 
 
 def test_balanced_accuracy():
     y_true = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 1.0
+    assert np.isclose(balanced_accuracy(y_true, y_pred), 1.0)
 
     y_true = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 0.0
+    assert np.isclose(balanced_accuracy(y_true, y_pred), 0.0)
 
     y_true = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.balanced_accuracy_score(y_true, y_pred) == 1/3
+    assert np.isclose(balanced_accuracy(y_true, y_pred), 1/3)
 
 
 def test_f1():
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
-    assert sklearn.metrics.f1_score(y_true, y_pred, average='weighted') == 1.0
+    assert np.isclose(f1(y_true, y_pred), 1.0)
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.f1_score(y_true, y_pred, average='weighted') == 0.0
+    assert np.isclose(f1(y_true, y_pred), 0.0)
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.f1_score(y_true, y_pred, average='weighted') == 1/3
+    assert np.isclose(f1(y_true, y_pred), 1/3)
 
     y_true = np.array([1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 0, 0, 1, 1, 0, 0, 0])
-    assert sklearn.metrics.f1_score(y_true, y_pred, average='weighted') == 0.6
+    assert np.isclose(f1(y_true, y_pred), 0.6)
 
     y_true = np.array([2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0])
     y_pred = np.array([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
-    assert sklearn.metrics.f1_score(y_true, y_pred, average='weighted') == 1/6
+    assert np.isclose(f1(y_true, y_pred), 1/6)
