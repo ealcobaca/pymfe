d33da8b1927c3bf2897d2cbdff0275af0601f16e
e.alcobaca <e.alcobaca@gmail.com>
2019-09-06 16:41:13 -0300

    New version 0.1.0

--- pymfe/_version.py
+++ pymfe/_version.py
@@ -20,6 +20,6 @@ in machine learning and AutoML.
 #
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
-#
+
 
 __version__ = '0.1.0'
