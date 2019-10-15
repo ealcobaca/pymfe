5ddd4766b666fe02bd2f6ca2b82dedbd2974500a
ealcobaca <e.alcobaca@gmail.com>
2019-10-15 02:34:46 -0300

    update clustering

--- pymfe/clustering.py
+++ pymfe/clustering.py
@@ -445,7 +445,7 @@ class MFEClustering:
 
         representative = np.array(representative)
 
-        num_repr, repr_dim = representative.shape
+        num_repr, repr_dim = np.shape(representative)
         _, num_attr = N.shape
 
         if num_repr != classes.size:
