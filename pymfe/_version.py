281f08b88bfda8448d96c8a990e94241a99d5b0b
ealcobaca <e.alcobaca@gmail.com>
2020-05-18 06:08:59 -0300

    Alpha release candidate -- 0.3

--- pymfe/_version.py
+++ pymfe/_version.py
@@ -21,4 +21,4 @@ in machine learning and AutoML.
 # Dev branch marker is: 'X.Y.dev' or 'X.Y.devN' where N is an integer.
 # 'X.Y.dev0' is the canonical version of 'X.Y.dev'
 
-__version__ = '0.3.0'
+__version__ = '0.3.a0'
