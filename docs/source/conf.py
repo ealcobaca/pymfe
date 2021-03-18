fa192e1459ef2253572f637fbc5f32cb66b25375
ealcobaca <e.alcobaca@gmail.com>
2021-03-18 18:49:48 -0300

    Update formatting with Black

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@ import sphinx_rtd_theme
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.insert(0, os.path.abspath('../sphinxext'))
+sys.path.insert(0, os.path.abspath("../sphinxext"))
 from github_link import make_linkcode_resolve
 import sphinx_gallery
 
@@ -27,24 +27,25 @@ author = "Edesio Alcobaça, Felipe Siqueira"
 
 # The full version, including alpha/beta/rc tags.
 from pymfe import __version__
+
 version = release = __version__
 
 
 # -- General configuration ---------------------------------------------------
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.linkcode',
-    'sphinx.ext.intersphinx',
-    'sphinx_gallery.gen_gallery',
-    'numpydoc',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.linkcode",
+    "sphinx.ext.intersphinx",
+    "sphinx_gallery.gen_gallery",
+    "numpydoc",
 ]
 
 # see: https://github.com/phn/pytpm/issues/3#issuecomment-12133978
@@ -53,7 +54,7 @@ numpydoc_show_class_members = False
 
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # If true, '()' will be appended to :func: etc. cross-reference text.
 add_function_parentheses = False
@@ -61,26 +62,25 @@ add_function_parentheses = False
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', '_templates']
+exclude_patterns = ["_build", "_templates"]
 # exclude_patterns = []
 
-autodoc_default_options = {'members': True, 'inherited-members': True}
+autodoc_default_options = {"members": True, "inherited-members": True}
 # autodoc_default_flags = ['members', 'inherited-members']
 
 # generate autosummary even if no references
 autosummary_generate = True
 
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = "sphinx_rtd_theme"
 # html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
@@ -91,33 +91,35 @@ html_theme = 'sphinx_rtd_theme'
 
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'pymfe-docs'
+htmlhelp_basename = "pymfe-docs"
 
 
 # intersphinx configuration
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/{.major}'.format(
-        sys.version_info), None),
-    'numpy': ('https://docs.scipy.org/doc/numpy/', None),
-    'scipy': ('https://docs.scipy.org/doc/scipy/reference', None),
-    'matplotlib': ('https://matplotlib.org/', None),
-    'sklearn': ('http://scikit-learn.org/stable', None),
-    'pandas': ('https://pandas.pydata.org/pandas-docs/stable/', None),
-    'arff': ('https://pythonhosted.org/liac-arff/', None)
+    "python": (
+        "https://docs.python.org/{.major}".format(sys.version_info),
+        None,
+    ),
+    "numpy": ("https://docs.scipy.org/doc/numpy/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/reference", None),
+    "matplotlib": ("https://matplotlib.org/", None),
+    "sklearn": ("http://scikit-learn.org/stable", None),
+    "pandas": ("https://pandas.pydata.org/pandas-docs/stable/", None),
+    "arff": ("https://pythonhosted.org/liac-arff/", None),
 }
 
 # ------ sphinx gallery ----------------------------------------------------
 
 sphinx_gallery_conf = {
-    'doc_module': 'pymfe',
-    'backreferences_dir': os.path.join('generated'),
+    "doc_module": "pymfe",
+    "backreferences_dir": os.path.join("generated"),
     # path to your example scripts
-    'examples_dirs': '../../examples',
+    "examples_dirs": "../../examples",
     # path where to save gallery generated examples
-    'gallery_dirs': 'auto_examples',
-    'reference_url': {
+    "gallery_dirs": "auto_examples",
+    "reference_url": {
         # The module you locally document uses None
-        'pymfe': None,
+        "pymfe": None,
     },
     # 'thumbnail_size': (50, 50),
     # 'junit': '../test-results/sphinx-gallery/junit.xml',
@@ -128,17 +130,19 @@ sphinx_gallery_conf = {
 }
 
 
-
 # --- GitHun link -----------------------------------------------------------
 
 # The following is used by sphinx.ext.linkcode to provide links to github
-linkcode_resolve = make_linkcode_resolve('pymfe',
-                                         u'https://github.com/ealcobaca/'
-                                         'pymfe/blob/{revision}/'
-                                         '{package}/{path}#L{lineno}')
+linkcode_resolve = make_linkcode_resolve(
+    "pymfe",
+    u"https://github.com/ealcobaca/"
+    "pymfe/blob/{revision}/"
+    "{package}/{path}#L{lineno}",
+)
 # print(linkcode_resolve('py', {'module': 'pymfe.mfe', 'fullname': 'MFE'}))
 
 
 # run auto pages
 import os
-os.system('python pages/run.py')
+
+os.system("python pages/run.py")
