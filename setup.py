9df37f0ff20cb6a28beb6718ddc98a58942e5150
felsiq <felipe.siqueira@usp.br>
2022-10-03 18:21:54 -0300

    setup: add 'igraph' and 'gower' as deps

--- setup.py
+++ setup.py
@@ -4,72 +4,67 @@ import os
 import pymfe
 
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
 
 
-NAME = 'pymfe'
+NAME = "pymfe"
 
 
 VERSION = pymfe.__version__
 
 
-DESCRIPTION = 'Meta-feature Extractor'
+DESCRIPTION = "Meta-feature Extractor"
 
 
-LICENSE = 'MIT'
+LICENSE = "MIT"
 
 
-URL = 'https://github.com/ealcobaca/pymfe'
+URL = "https://github.com/ealcobaca/pymfe"
 
-MAINTAINER = 'Edesio Alcobaça, Felipe Alves Siqueira'
-
-
-MAINTAINER_EMAIL = 'edesio@usp.br, felipe.siqueira@usp.br'
+MAINTAINER = "Edesio Alcobaça, Felipe Alves Siqueira"
 
 
-DOWNLOAD_URL = 'https://github.com/ealcobaca/pymfe/releases'
+MAINTAINER_EMAIL = "edesio@usp.br, felipe.siqueira@usp.br"
 
 
-CLASSIFIERS = ['Intended Audience :: Science/Research',
-               'Intended Audience :: Developers',
-               'License :: OSI Approved :: MIT License',
-               'Natural Language :: English',
-               'Programming Language :: Python',
-               'Topic :: Software Development',
-               'Topic :: Scientific/Engineering',
-               'Operating System :: OS Independent',
-               'Programming Language :: Python :: 3.6',
-               'Programming Language :: Python :: 3.7',
-               'Programming Language :: Python :: 3.8',
-               'Programming Language :: Python :: 3.9']
+DOWNLOAD_URL = "https://github.com/ealcobaca/pymfe/releases"
 
 
-INSTALL_REQUIRES = ['numpy', 'scipy', 'scikit-learn', 'patsy', 'pandas',
-                    'statsmodels', 'texttable', 'tqdm']
+CLASSIFIERS = [
+    "Intended Audience :: Science/Research",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Scientific/Engineering",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+]
+
+
+INSTALL_REQUIRES = [
+    "numpy",
+    "scipy",
+    "scikit-learn",
+    "patsy",
+    "pandas",
+    "statsmodels",
+    "texttable",
+    "tqdm",
+    "igraph>=0.10.1",
+    "gower",
+]
 
 
 EXTRAS_REQUIRE = {
-    'code-check': [
-        'pytest',
-        'mypy',
-        'liac-arff',
-        'flake8',
-        'pylint'
-    ],
-    'tests': [
-        'pytest',
-        'pytest-cov',
-        'pytest-xdist',
-        'liac-arff'
-    ],
-    'docs': [
-        'sphinx',
-        'sphinx-gallery',
-        'sphinx_rtd_theme',
-        'numpydoc',
-        'liac-arff'
-    ]
+    "code-check": ["pytest", "mypy", "liac-arff", "flake8", "pylint"],
+    "tests": ["pytest", "pytest-cov", "pytest-xdist", "liac-arff"],
+    "docs": ["sphinx", "sphinx-gallery", "sphinx_rtd_theme", "numpydoc", "liac-arff"],
 }
 
 
